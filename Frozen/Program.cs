3eac6a9b5e8e00f00e7769f5daa5c4c19ea401ab
artjombokov <abokov@tktk.ee~>
2022-05-11 12:57:26 +0300

    new project

---
+++ Frozen/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        class Frozen
+        {
+            string name;
+            string present;
+
+            public Frozen(string _name, string _present)
+            {
+                name = _name;
+                present = _present;
+            }
+            //getters for SecretSanta
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Present
+            {
+                get { return present; }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            //Anna wants new earrings for Christmas
+
+            List<Frozen> myFrozen = new List<Frozen>();
+            string[] frozenFromFile = GetDataFromFile();
+
+            foreach (string line in frozenFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                Frozen newFrozen = new Frozen(tempArray[0], tempArray[1]);
+                myFrozen.Add(newFrozen);
+            }
+
+            foreach (Frozen frozenFromList in myFrozen)
+            {
+                Console.WriteLine($"{frozenFromList.Name} wants {frozenFromList.Present}");
+            }
+        }
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach (string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\...\samplestktk\frozen";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+}
